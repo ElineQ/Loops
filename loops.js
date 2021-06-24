69e128a758e197b578d51f92cff4d7b04f5ed5e0
ElineQ <84079851+ElineQ@users.noreply.github.com>
2021-06-24 14:22:03 +0200

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,38 @@
+for (let i = 0; i <= 20; i++) {
+    while (i = odd) (
+        console.log("This number us odd")
+    )
+
+    console.log(`Is ${i} even or odd?`);
+    // TODO: determine if i is odd or even
+    // TODO: use console.log
+}
+
+document.write("This number is even");
+
+var a = 9;
+var b;
+
+for (var i = 1; i <= 9; i++) {
+    b = a * i;
+
+    document.write(" " + a + "*" i "*", * b);
+}
+
+
+function assignGrade(score) {
+    var score = 80
+
+    if (score > 100) {
+        return "A";
+    } else if (score > 90) {
+        return "B";
+    } else if (score > 80) {
+        return "C";
+    } else if (score > 70) {
+        return "D";
+    } else {
+        (score > 70)
+        return "E";
+    }
+}
